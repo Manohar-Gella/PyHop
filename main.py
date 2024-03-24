84ea34762f3c280a3499a633213650b6cc626aba
Manohar Gella <manohargella272@gmail.com>
2024-03-24 23:58:13 +0530

    python

---
+++ main.py
@@ -0,0 +1,35 @@
+import random
+import time
+
+seats = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+         11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+def remove_element(array, element):
+    if element in array:
+        array.remove(element)
+def generate_random_5_digit_number():
+    return random.randint(10000, 99999)
+
+def ticket(name, age, nseats):
+    print(f"Name of the passenger {name.upper()}")
+    print(f"Age of the passenger {age}")
+    print(f"Allotted seat number {nseats}")
+    print("Happy journey")
+    print("Ticket number")
+    random_number = generate_random_5_digit_number()
+    print(random_number)
+
+def main():
+    while True:
+        print("Railway Seat Reservation Portal")
+        name = input("Name of the passenger: ")
+        age = int(input("Enter age: "))
+        print(seats)
+        nseats = int(input("Enter preferred seats from the list: "))
+        print("Generating tick")
+        time.sleep(3)
+        remove_element(seats, nseats)
+        ticket(name, age, nseats)
+
+main()
+
